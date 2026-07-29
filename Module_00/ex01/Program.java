f913302ad09f55369d0ba0468c44346376a0fc65
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-07-29 12:36:59 +0100

    Exit with code -1 when number is invalid

--- Module_00/ex01/Program.java
+++ Module_00/ex01/Program.java
@@ -3,24 +3,23 @@ import java.util.Scanner;
 class Program {
     public static void main ( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number");
+        System.out.printf("->");
         if ( !scanner.hasNextInt() ) {
             System.out.println( "IllegalArgument" );
-            System.exit(1);
+            System.exit(-1);
         }
         int         nb  = scanner.nextInt();
         if ( nb < 2 ) {
             System.out.println( "IllegalArgument" );
-            System.exit(1);
+            System.exit(-1);
         }
         boolean     x   = true;
         int         i   = 2;
-        for ( ; ; i++ ) {
+        for ( ; i * i <= nb; i++ ) {
             if ( nb % i == 0 ) {
                 x = false;
                 break;
             }
-            if ( i * i >= nb ) break;
         }
         System.out.printf( "%b %d" ,x , i - 1);
     }
